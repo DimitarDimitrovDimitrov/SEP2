ad0aa444739fc1c17adbc2361a24c2d7dc499d4f
Dimitar <Dimitar_Dimitrov_Dimitrov@yahoo.com>
2016-11-28 12:55:30 +0100

    implemented cLient and ClientInterface

--- src/Client/client.java
+++ src/Client/client.java
@@ -6,11 +6,27 @@ import java.rmi.server.UnicastRemoteObject;
 public class client extends UnicastRemoteObject implements ClientInterface
 {
 private String name;
-private ChatUI ui;
+//private ChatUI ui;
 public client(String n) throws RemoteException{
 	name=n;
 }
+@Override
+public void tell(String st) throws RemoteException {
+	System.out.println(st);
+	//ui.TypeMessage(st);
 	
+}
+@Override
+public String getName() throws RemoteException {
+	return name;
+	
+}
+
+/* 
+ * 	public void setGUI(ChatUI t){
+ * ui=t;
+ * }
+ */
 	
 	
 }
