5ab9858897f8631f8d0733031649d80875f2cc0e
Dimitar <Dimitar_Dimitrov_Dimitrov@yahoo.com>
2016-11-28 12:48:57 +0100

    Implemented ClientInterface and client

--- src/Client/client.java
+++ src/Client/client.java
@@ -1,6 +1,16 @@
 package Client;
 
-public class client
-{
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
+public class client extends UnicastRemoteObject implements ClientInterface
+{
+private String name;
+private ChatUI ui;
+public client(String n) throws RemoteException{
+	name=n;
+}
+	
+	
+	
 }
