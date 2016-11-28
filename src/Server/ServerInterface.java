36ae5a8190102d6f99c4d16445ad4bca1dd8dfe6
Todor Gerganov <Todor Gerganov>
2016-11-28 13:05:54 +0100

    Status 28Nov 13.05

--- src/Server/ServerInterface.java
+++ src/Server/ServerInterface.java
@@ -3,9 +3,11 @@ package Server;
 import java.rmi.*;
 import java.util.*;
 
+import Client.ClientInterface;
+
 public interface ServerInterface extends Remote
 {
+   public boolean login (ClientInterface a) throws RemoteException;
    public void publish (String s) throws RemoteException; 
-   
-
+   ArrayList<String> getConnected = new ArrayList<String>();
 }
