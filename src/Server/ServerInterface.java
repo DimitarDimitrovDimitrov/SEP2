c12816b27087b031dc21464e88c6033623eb0a42
Todor Gerganov <Todor Gerganov>
2016-11-28 12:27:00 +0100

    State 28Nov 12.26

--- src/Server/ServerInterface.java
+++ src/Server/ServerInterface.java
@@ -1,6 +1,11 @@
 package Server;
 
-public interface ServerInterface
+import java.rmi.*;
+import java.util.*;
+
+public interface ServerInterface extends Remote
 {
+   public void publish (String s) throws RemoteException; 
+   
 
 }
