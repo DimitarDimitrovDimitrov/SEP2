1458b3c81c51d8dbce562ec7041bb16853dc8361
odinchev <oleg_dinchev@abv.bg>
2016-12-06 14:05:51 +0200

    added code

---
+++ src/Adapter/Adapter.java
@@ -0,0 +1,27 @@
+package Adapter;
+
+public class Adapter implements AdapterInterface
+{
+
+   private static final String DB_NAME = "Sep2";
+   
+   public Adapter()
+   {
+      
+   }
+
+   @Override
+   public String Write()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String Read()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+  
+}
