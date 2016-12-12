5ac0916e840602110e52c4a0b3deac5b32ef4f3b
odinchev <oleg_dinchev@abv.bg>
2016-12-12 17:08:26 +0200

    added code

--- src/ChatSystem/MessageBroadcast.java
+++ src/ChatSystem/MessageBroadcast.java
@@ -2,7 +2,16 @@ package ChatSystem;
 
 import java.awt.List;
 import java.util.ArrayList;
-//coooment
+
+/**
+ * @author Oleg,Dimitar,Todor.
+ * <p>
+ *  This is the MessageBroadcast class it provides a list used to store or retrieve connections and show the number of connected clients.
+ * <p>
+ * 
+ *  
+ * 
+ */
 public class MessageBroadcast
 {
    private ArrayList <ServerConnection>conns=new ArrayList<>();
@@ -10,14 +19,42 @@ public class MessageBroadcast
    {
       
    }
+   /**
+    * Adds a connection to the connections list                         (1)
+    * <p>
+    *  Adds a connection to the connectionlist conns 
+    * <p>
+    * 
+    * @param  ServerConnection conn. 
+    * 
+    */
+
    public void addConnection(ServerConnection conn)
    {
       conns.add(conn);
    }
+   /**
+    * returns the number of clients that in the conns  .                   (1)
+    * <p>
+    *  returns the number of clients stored in   conns .
+    * <p>
+    * 
+    * @return conns.size();
+    * 
+    */
    public int numberofclients()
    {
       return conns.size();
    }
+   /**
+    * returns the connection stored at index i  .                   
+    * <p>
+    *  returns the number of clients stored in   conns .
+    * <p>
+    * @param int i.
+    * @return conns.get(i);
+    * 
+    */
    public ServerConnection getConnection(int i)
    {
       return conns.get(i);
